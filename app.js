a47cf1e71a1077abe575c3d77b8ca048871abae8
unknown <JNKarif>
2023-12-04 11:56:11 +0600

    seach box function added

--- app.js
+++ app.js
@@ -3,17 +3,47 @@
 
 const apiKey = "f0bbd31e7c47cf24fed175d28afe4139";
 const apiUrl =
-  "https://api.openweathermap.org/data/2.5/weather?&units=metric&q=dhaka"; /*We have to change the units for degree in units=metric*/
+  "https://api.openweathermap.org/data/2.5/weather?&units=metric&q="; /*We have to change the units for degree in units=metric*/
 
-async function checkWeather() {
-  const res = await fetch(apiUrl + `&appid=${apiKey}`);
+const searchBox = document.querySelector(".search input");
+/**to get weather information by search
+step 1: delete city name from url>>> add city as parameter>>> add city parameter in fetch url with +
+step 2: DOM from search class >>> and .search input
+step 3: addEventListener>>>to searchBox because from input we will get data
+step 4: call the function inside addEventListener and give the city parameter argument as searchBox.value because value is the builtin
+*/
+const searchBtn = document.querySelector(".search button");
+
+async function checkWeather(city) {
+  const res = await fetch(apiUrl + city + `&appid=${apiKey}`);
+  /**changing dynamically the fetch url
+   * &appid= will add to the url dynamically for ``back tick
+   * city will be added with url dynamically
+   */
   var data = await res.json();
   console.log(data);
 
   document.querySelector(".city").innerHTML = data.name;
-  document.querySelector(".temp").innerHTML = data.main.temp;
-  document.querySelector(".humidity").innerHTML = data.main.humidity;
-  document.querySelector(".wind").innerHTML = data.wind.speed;
+  document.querySelector(".temp").innerHTML =
+    Math.round(data.main.temp) +
+    "°c"; /*Math.round() to make the floating number to integer */
+  document.querySelector(".humidity").innerHTML = data.main.humidity + " %";
+  document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
 }
 
-checkWeather();
+searchBtn.addEventListener("click", () => {
+  checkWeather(searchBox.value);
+});
+
+/**
+ * To apply Enter key:
+ * "keadown" addEventListener
+ * take a parameter
+ * if condition parameter.key === the given key name
+ * call the function inside if condition
+ */
+searchBox.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    checkWeather(searchBox.value);
+  }
+});
