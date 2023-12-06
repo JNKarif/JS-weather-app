34ad18dd3ff761d4ea03d9ee89f161470a398e17
unknown <JNKarif>
2023-12-06 10:55:35 +0600

    final coding

--- app.js
+++ app.js
@@ -17,31 +17,40 @@ const searchBtn = document.querySelector(".search button");
 const weatherIcon = document.querySelector(".weather-icon");
 
 async function checkWeather(city) {
-  const res = await fetch(apiUrl + city + `&appid=${apiKey}`);
+  const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
   /**changing dynamically the fetch url
    * &appid= will add to the url dynamically for ``back tick
    * city will be added with url dynamically
    */
-  var data = await res.json();
-  console.log(data);
+  if (response.status == 404) {
+    document.querySelector(".error").style.display = "block";
+    document.querySelector(".weather").style.display = "none";
+  } else {
+    var data = await response.json();
+    console.log(data);
 
-  document.querySelector(".city").innerHTML = data.name;
-  document.querySelector(".temp").innerHTML =
-    Math.round(data.main.temp) +
-    "°c"; /*Math.round() to make the floating number to integer */
-  document.querySelector(".humidity").innerHTML = data.main.humidity + " %";
-  document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML =
+      Math.round(data.main.temp) +
+      "°c"; /*Math.round() to make the floating number to integer */
+    document.querySelector(".humidity").innerHTML = data.main.humidity + " %";
+    document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
 
-  if (data.weather[0].main == "Clouds") {
-    weatherIcon.src = "Images/clouds.png";
-  } else if (data.weather[0].main == "Clear") {
-    weatherIcon.src = "Images/clear.png";
-  } else if (data.weather[0].main == "Drizzle") {
-    weatherIcon.src = "Images/drizzle.png";
-  } else if (data.weather[0].main == "Mist") {
-    weatherIcon.src = "Images/mist.png";
-  } else if (data.weather[0].main == "Snow") {
-    weatherIcon.src = "Images/snow.png";
+    // dynamic weather image implement
+    if (data.weather[0].main == "Clouds") {
+      weatherIcon.src = "Images/clouds.png";
+    } else if (data.weather[0].main == "Clear") {
+      weatherIcon.src = "Images/clear.png";
+    } else if (data.weather[0].main == "Drizzle") {
+      weatherIcon.src = "Images/drizzle.png";
+    } else if (data.weather[0].main == "Mist") {
+      weatherIcon.src = "Images/mist.png";
+    } else if (data.weather[0].main == "Snow") {
+      weatherIcon.src = "Images/snow.png";
+    }
+
+    document.querySelector(".weather").style.display = "block";
+    document.querySelector(".error").style.display = "none";
   }
 }
 
